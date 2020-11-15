11bf1ed5309ab667fce74cfc1b1521e81231e33b
HLQ_Struggle <925954424@qq.com>
2020-11-15 12:19:06 +0800

    Add LeetCode Q14 优化解法

--- src/com/hlq/leetcode/array/Question14.kt
+++ src/com/hlq/leetcode/array/Question14.kt
@@ -12,9 +12,41 @@ fun main() {
     val strs1 = arrayOf("flower", "flower", "flower", "flower")
     val strsNull = arrayOf("dog", "racecar", "car")
 
+    // 暴力解法 1
     println("----> ${longestCommonPrefix(strs)}")
     println("----> ${longestCommonPrefix(strs1)}")
     println("----> ${longestCommonPrefix(strsNull)}")
+
+    // 暴力解法 2
+    println("----> ${longestCommonPrefix2(strs)}")
+}
+
+/**
+ * 暴力解法 2
+ */
+fun longestCommonPrefix2(strs: Array<String>): String {
+    var resultStr = ""
+    if (strs.isEmpty()) {
+        return resultStr
+    }
+    if (strs.size == 1) {
+        return strs[0]
+    }
+    resultStr = strs[0]
+    for(i in 1 until strs.size){
+        var j = 0
+        while (j < resultStr.length && j < strs[i].length){
+            if(strs[i].toCharArray()[j] != resultStr.toCharArray()[j]){
+                break
+            }
+            j++
+        }
+        resultStr = resultStr.substring(0,j)
+        if(resultStr.isEmpty()){
+            break
+        }
+    }
+    return resultStr
 }
 
 /**
