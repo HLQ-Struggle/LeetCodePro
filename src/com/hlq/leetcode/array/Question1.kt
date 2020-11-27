d516a35963481bcd0b07344264d1b00d1b0c05ca
HLQ_Struggle <925954424@qq.com>
2020-11-28 00:46:22 +0800

    Add LeetCode 1 优化解法

--- src/com/hlq/leetcode/array/Question1.kt
+++ src/com/hlq/leetcode/array/Question1.kt
@@ -10,12 +10,15 @@ fun main() {
     nums[0] = 3
     nums[1] = 2
     nums[2] = 4
-//    nums[3] = 15
 
     val target = 6
 
     // 暴力解法 一
     twoSum(nums, target)
+
+    // 优化
+    twoSum2(nums, target)
+    twoSum3(nums, target)
 }
 
 /**
@@ -47,4 +50,49 @@ fun twoSum(nums: IntArray, target: Int): IntArray {
         j = i + 1
     }
     return resultArray
+}
+
+/**
+ * 优化 一
+ */
+fun twoSum2(nums: IntArray, target: Int): IntArray {
+    // 基本校验
+    if (nums.isEmpty()) {
+        return IntArray(0)
+    }
+    var i = 0
+    var j = 1
+    while (i < nums.size - 1) {
+        while (j < nums.size) {
+            if (nums[i] + nums[j] == target) {
+                return intArrayOf(i, j)
+            }
+            j++
+        }
+        i++
+        j = i + 1
+    }
+    return intArrayOf(0, 0)
+}
+
+/**
+ * 继续优化
+ */
+fun twoSum3(nums: IntArray, target: Int): IntArray {
+    if (nums.isEmpty()) {
+        return intArrayOf(0, 0)
+    }
+    var i = 0
+    var j = 1
+    while (i < nums.size - 1) {
+        while (j < nums.size) {
+            if (nums[i] + nums[j] == target) {
+                return intArrayOf(i, j)
+            }
+            j++
+        }
+        i++
+        j = i + 1
+    }
+    return intArrayOf(0, 0)
 }
