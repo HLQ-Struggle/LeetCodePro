9c09e992b6d4cfeee0a61f982bcd3abd10061cea
HLQ_Struggle <925954424@qq.com>
2020-11-28 00:28:44 +0800

    Add LeetCode 1 双指针暴力解法

--- src/com/hlq/leetcode/array/Question1.kt
+++ src/com/hlq/leetcode/array/Question1.kt
@@ -6,5 +6,45 @@ package com.hlq.leetcode.array
  * @desc LeetCode 1. 两数之和 https://leetcode-cn.com/problems/two-sum/
  */
 fun main() {
+    val nums = IntArray(3)
+    nums[0] = 3
+    nums[1] = 2
+    nums[2] = 4
+//    nums[3] = 15
 
+    val target = 6
+
+    // 暴力解法 一
+    twoSum(nums, target)
+}
+
+/**
+ * 双指针解法
+ */
+fun twoSum(nums: IntArray, target: Int): IntArray {
+    // 基本校验
+    if (nums.isEmpty()) {
+        return IntArray(0)
+    }
+    var resultArray = IntArray(2)
+    var isl = false
+    var i = 0
+    var j = 1
+    while (i < nums.size - 1) {
+        while (j < nums.size) {
+            if (nums[i] + nums[j] == target) {
+                resultArray[0] = i
+                resultArray[1] = j
+                isl = true
+                break
+            }
+            j++
+        }
+        if (isl) {
+            break
+        }
+        i++
+        j = i + 1
+    }
+    return resultArray
 }
