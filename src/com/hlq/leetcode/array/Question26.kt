d6f09708b0c688e58861d3fabb349078994cf0d7
HLQ_Struggle <925954424@qq.com>
2020-11-30 23:40:30 +0800

    Add LeetCode 26 LeetCode 范例

--- src/com/hlq/leetcode/array/Question26.kt
+++ src/com/hlq/leetcode/array/Question26.kt
@@ -19,11 +19,46 @@ fun removeDuplicates(nums: IntArray): Int {
     if(nums.isEmpty()){
         return 0
     }
-    var result = 0
+    var resultCount = 0
     for(i in nums.indices){
-        if(nums[i] != nums[result]){
-            nums[++result] = nums[i]
+        if(nums[i] != nums[resultCount]){
+            nums[++resultCount] = nums[i]
         }
     }
-    return ++result
+    return ++resultCount
+}
+
+/**
+ * LeetCode 执行用时范例
+ */
+fun removeDuplicates1(nums: IntArray): Int {
+    if (nums.isEmpty()) return 0
+    var i = 0
+    for (j in 1 until nums.size) {
+        if (nums[j] != nums[i]) {
+            i++
+            nums[i] = nums[j]
+        }
+    }
+    return i + 1
+}
+
+/**
+ * LeetCode 执行消耗内存范例
+ */
+fun removeDuplicates2(nums: IntArray): Int {
+    if(nums == null){
+        return 0
+    }
+    if(nums.size == 1){
+        return 1
+    }
+    var i = 0
+    for(j in 1 until nums.size){
+        if(nums[i] != nums[j]){
+            i++
+            nums[i] = nums[j]
+        }
+    }
+    return i+1
 }
